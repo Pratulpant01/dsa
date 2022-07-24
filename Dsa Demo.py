7819d520608d0fc30f4e5d823e00eee99ec9af20
Pratul Pant <pratulpant02@gmail.com>
2022-07-25 01:21:15 +0530

    dsa practice

---
+++ Dsa Demo.py
@@ -0,0 +1,19 @@
+import array
+
+arr = array.array('i', [1,2,3])
+
+print('The new created array is:', end=' ')
+
+for j in range(0,3):
+    print(arr[j], end= ' ')
+
+arr.append(4)
+
+print('The appended array is:', end=' ')
+
+for i in range(0,4):
+    print(arr[i], end= ' ')
+
+
+#Linked List
+
---
+++ demo.py
@@ -0,0 +1,4 @@
+a= {'Hitesh': 20, 'Pratul': 30, 'Rekha': 40, 'Mahesh':{'Rupees': 50, 'Age':56, 'Year': 1956}, 'Nishant': 100}
+print(a['Mahesh']['Year'])
+print(a['Hitesh'])
+print(a['Mahesh'])
