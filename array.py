7b6584442b117758bbc520f1e3a5856da176df40
Pratul Pant <pratulpant02@gmail.com>
2022-07-27 06:57:45 +0530

    linked list

--- array.py
+++ array.py
@@ -1,82 +1,82 @@
-#1D Array
+# 1D Array
 
-# print('How many elements to store inside an array', end=" ")
-# num= input()
-# arr = []
-# print('\nEnter', num, 'Element:', end= '' )
-# num = int(num)
-# for i in range (num):
-#     element = input()
-#     arr.append(element)
-# print('\n The array elements are: ')
-# for element in arr:
-#     print(element, end= '')
-
-
-#2D array
-
-# r_num= int(input('Enter number of rows'))
-# c_num = int(input('Enter number of columns'))
-# twod_arr= [[5 for col in range(c_num)] for row in range(r_num)]
-
-# for row in range(r_num):
-#     for col in range(c_num):
-#         twod_arr[row][col] = row*col
-# print(twod_arr)
+print('How many elements to store inside an array', end=" ")
+num= input()
+arr = []
+print('\nEnter', num, 'Element:', end= '' )
+num = int(num)
+for i in range (num):
+    element = input()
+    arr.append(element)
+print('\n The array elements are: ')
+for element in arr:
+    print(element, end= '')
 
 
-# #Implement searching inside an array
+# 2D array
 
-# #Delete element inside an array
+r_num= int(input('Enter number of rows'))
+c_num = int(input('Enter number of columns'))
+twod_arr= [[5 for col in range(c_num)] for row in range(r_num)]
 
-# print('Enter the size of an array')
-
-# tot = int(input())
-# arr = []
-
-# print('Enter elements in your array2:') 
+for row in range(r_num):
+    for col in range(c_num):
+        twod_arr[row][col] = row*col
+print(twod_arr)
 
 
-# for i in range(tot):
+#Implement searching inside an array
 
-#     arr.append(input())
-# print('Enter the value to delete:') 
+#Delete element inside an array
 
-# val = input()
+print('Enter the size of an array')
 
-# if val in arr:
-#     arr.remove(val)
-#     print('The new array is:')
-#     for i in range(tot-1):
-#         print(arr[i])
-# else:
-#     print('Element doesnt exist')
+tot = int(input())
+arr = []
+
+print('Enter elements in your array2:') 
+
+
+for i in range(tot):
+
+    arr.append(input())
+print('Enter the value to delete:') 
+
+val = input()
+
+if val in arr:
+    arr.remove(val)
+    print('The new array is:')
+    for i in range(tot-1):
+        print(arr[i])
+else:
+    print('Element doesnt exist')
 
 # Sort array in python
 
-# from operator import le
+from operator import le
 
 
-# arr = [12,102, 25, 45, 32, 67, 89]
-# temp = 0
+arr = [12,102, 25, 45, 32, 67, 89]
+temp = 0
 
-# print("Elements of an original array is:")
+print("Elements of an original array is:")
 
-# for i in range(len(arr)):
-#     print(arr[i])
-# #Sorting
+for i in range(len(arr)):
+    print(arr[i])
+#Sorting
 
-# for i in range(len(arr)):
-#     for j in range(i+1, len(arr)):
-#         if arr[i]> arr[j]:
-#             temp = arr[i]
-#             arr[i] = arr[j]
-#             arr[j] = temp
-# print()
+for i in range(len(arr)):
+    for j in range(i+1, len(arr)):
+        if arr[i]> arr[j]:
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+print()
 
-# print('Elements sorted in ascending order are:')
-# for i in range(len(arr)):
-#     print(arr[i])
+print('Elements sorted in ascending order are:')
+for i in range(len(arr)):
+    print(arr[i])
 
 
 # Search and see elements in an array
---
+++ linkedlist.py
@@ -0,0 +1,51 @@
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+
+
+# n1 = Node(5)
+# print(n1.data)
+# print(n1.next)
+        
+# #creating a linked list
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
+
+# sll = LinkedList()
+
+#Traversal in LinkedList
+
+from time import sleep
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def traversal(self):
+        if self.head is None:
+            print('Singly Linked List is empty')
+        else:
+            a = self.head  # Creating a temp variable to assign self.head so that it is not changed
+            while a is not None:
+                print(a.data, end= "=>")
+                a = a.next
+
+n1= Node(4)
+sll = LinkedList()
+sll.head = n1
+n2 = Node(5)
+n1.next = n2
+n3 = Node(10)
+n2.next = n3
+n4 = Node(12)
+n3.next = n4
+
+sll.traversal()
