8bf37a49cbe420a608d19c61bdec9c1f6930f745
Pratul Pant <pratulpant02@gmail.com>
2022-08-07 23:06:05 +0530

    update

--- question.py
+++ question.py
@@ -13,15 +13,15 @@
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevNum = {}
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         prevNum = {}
         
-        for i, n in nums:
-            diff = target -n
-            if diff in prevNum:
-                return [prevNum[diff], i]
-            prevNum[n]= i
+#         for i, n in nums:
+#             diff = target -n
+#             if diff in prevNum:
+#                 return [prevNum[diff], i]
+#             prevNum[n]= i
         
 
 
@@ -38,25 +38,37 @@ class Solution:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy= ListNode()
-        cur= dummy
-        carry = 0
+# class Solution:
+#     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+#         dummy= ListNode()
+#         cur= dummy
+#         carry = 0
         
-        while l1 or l2 or carry:
-            v1 = l1.val if l1 else 0
-            v2= l2.val if l2 else 0
+# while l1 or l2 or carry:
+#     v1 = l1.val if l1 else 0
+#     v2= l2.val if l2 else 0
             
-            val = v1+v2+carry
-            carry = val //10
-            val = val%10
-            cur.next = ListNode(val)
-            cur = cur.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-        return dummy.next
+#     val = v1+v2+carry
+#     carry = val //10
+#     val = val%10
+#     cur.next = ListNode(val)
+#     cur = cur.next
+#     l1 = l1.next if l1 else None
+#     l2 = l2.next if l2 else None
+# return dummy.next
             
         
-
+# Given a string s, find the length of the longest substring without repeating characters.
+s= 'BAB'
+charSet = set()
+l= 0
+res = 0
+for r in range(len(s)):
+    print(r)
+    while s[r] in charSet:
+        print(s[r])
+        charSet.remove(s[l])
+        l+=1
+    charSet.add(s[r])
+    res = max(res, r-l +1)
     
