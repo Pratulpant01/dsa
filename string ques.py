36b84014bcdbacba5104f748eef0260b0a05cad7
Pratul Pant <pratulpant02@gmail.com>
2022-09-08 19:00:58 +0530

    practise

--- string ques.py
+++ string ques.py
@@ -7,3 +7,27 @@ def reverseWord(s):
         s1 = i+s1
     return s1
     
+# Parenthesis Checker
+
+def ispar(self,x):
+
+    def isOpening(c):
+        return (c == '(') or (c == '[') or (c == '{')
+    
+    def isClosing(a, b):
+        return (a == '(' and b == ')') or (a== '{' and b == '}') or (a == '[' and b == ']')
+    
+    s = []
+
+    for i in x:
+        if isOpening(i):
+            s.append(i)
+        else:
+            if not s:
+                return False
+            elif not isClosing(s[-1], i):
+                return False
+            else:
+                s.pop()
+    return True if not s else False
+            
