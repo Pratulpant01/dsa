cb07a0e61b39b5f2447100e5349a8a5fb4b32c70
Pratul Pant <pratulpant02@gmail.com>
2022-09-19 21:52:52 +0530

    practise

--- string ques.py
+++ string ques.py
@@ -619,3 +619,19 @@ def NumberofElementsInIntersection(self,a, b, n, m):
             i+=1
             j+=1
     return len(s)
+
+# Triplet Sum in Array
+def find3Numbers(self,A, n, X):
+    A.sort()
+
+    for i in range(0, n):
+        l= i+1
+        r = n-1
+        while l<r:
+            if A[i] + A[l] + A[r] == X:
+                return True
+            elif A[i] + A[l] + A[r] <X:
+                l+=1
+            else:
+                r-=1
+    return False
