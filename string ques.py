d7fd6351fdd50b38ced6f353f451c6f24f280be9
Pratul Pant <pratulpant02@gmail.com>
2022-09-20 21:28:34 +0530

    practise

--- string ques.py
+++ string ques.py
@@ -650,3 +650,25 @@ def equilibriumPoint(self,A, N):
         else:
             leftSum+= A[i]
     return -1
+
+# Smallest Positive missing number
+
+def missingNumber(self,arr,n):
+    for i in range(n):
+        if arr[i]<0:
+            arr[i] = 0
+    
+    for i in range(n):
+        val = arr[i]
+        if 1<= val <=n:
+            if arr[val-1] >0:
+                arr[val-1] *= -1
+            elif arr[val-1] == 0:
+                arr[val-1] *= -1 (n+1)
+    
+    for i in range(1, n+1):
+        if arr[i-1]>= 0:
+            return i
+    return n+1
+
+
