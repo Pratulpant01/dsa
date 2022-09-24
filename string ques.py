8051989887e5f0d416060b79f22411765eb8bbd6
Pratul Pant <pratulpant02@gmail.com>
2022-09-24 23:40:17 +0530

    practise

--- string ques.py
+++ string ques.py
@@ -799,3 +799,25 @@ def maxIndexDiff(self,arr, n):
         
     return maxi
 
+# 153. Find Minimum in Rotated Sorted Array
+
+
+def findMin(self, nums: List[int]) -> int:
+        n = len(nums)
+        res = nums[0]
+        l, r = 0, n-1
+        
+        while l<=r:
+            if nums[l]<nums[r]:
+                res = min(res, nums[l])
+                break
+            
+            m = (l+r) //2
+            
+            res = min(res, nums[m])
+            
+            if nums[m]>= nums[l]:
+                l = m+1
+            else:
+                r= m-1
+        return res
