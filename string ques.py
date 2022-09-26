494151631949b144c69e08c9ef1e287986120305
Pratul Pant <pratulpant02@gmail.com>
2022-09-26 23:20:20 +0530

    practise

--- string ques.py
+++ string ques.py
@@ -844,3 +844,28 @@ def minSwaps(self, nums):
             res[temp] = res[a[i]]
             res[a[i]] = i
     return swap
+
+# 33. Search in Rotated Sorted Array
+def search(self, nums: List[int], target: int) -> int:
+    n = len(nums)
+    l = 0
+    r = n-1
+
+    while l<=r:
+        mid = (l+r)//2
+
+        if nums[mid]== target:
+            return mid
+        
+        if nums[mid]>= nums[l]:
+            if target> nums[mid] or target< nums[l]:
+                l = mid +1
+            else:
+                r = mid-1
+
+        else:
+            if target<nums[mid] or target> nums[r]:
+                r = mid-1
+            else:
+                l = mid+1
+    return -1
