41946c1ffaa224798fb74a4ef33a2f69585814ad
Pratul Pant <pratulpant02@gmail.com>
2022-09-27 23:40:33 +0530

    practise

--- string ques.py
+++ string ques.py
@@ -890,3 +890,21 @@ def findMin(self, arr, n):
         else:
             r= mid-1
     return res
+
+# 11. Container With Most Water
+
+def maxArea(self, height: List[int]) -> int:
+    res = 0
+    n = len(height)
+    l=0
+    r = n-1
+    while l<r:
+        area = (r-l)* min(height[r], height[l])
+        res =  max(res, area)
+
+        if height[l]< height[r]:
+            l+=1
+        else:
+            r-=1
+    return res
+
