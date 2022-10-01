8e77f28dfe14931784c0aad3410744b831978e68
Pratul Pant <pratulpant02@gmail.com>
2022-10-01 23:42:01 +0530

    practise

--- string ques.py
+++ string ques.py
@@ -908,3 +908,27 @@ def maxArea(self, height: List[int]) -> int:
             r-=1
     return res
 
+
+# Parenthesis Checker
+
+def ispar(self,x):
+
+    def isOpening(c):
+        return (c == '(') or (c == '[') or (c == '{')
+    
+    def isClosing(a, b):
+        return (a == '(' and b == ')') or (a== '{' and b == '}') or (a == '[' and b == ']')
+    
+    s = []
+
+    for i in x:
+        if isOpening(i):
+            s.append(i)
+        else:
+            if not s:
+                return False
+            elif not isClosing(s[-1], i):
+                return False
+            else:
+                s.pop()
+    return True if not s else False
