fed41b10ec43bcc78d3c40134c0979cd801b2f0a
Pratul Pant <pratulpant02@gmail.com>
2022-10-06 23:32:39 +0530

    practise

--- string ques.py
+++ string ques.py
@@ -973,3 +973,27 @@ def reverseWords(self, s: str) -> str:
         right-=1
 
     return ' '.join(a)
+
+# 349. Intersection of Two Arrays
+
+def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        n = len(nums1)
+        m = len(nums2)
+        nums1.sort()
+        nums2.sort()
+
+        i = 0
+        j = 0
+        res = set()
+
+        while i<n and j<m:
+            if nums1[i]< nums2[j]:
+                i+=1
+            elif nums2[j]< nums1[i]:
+                j+=1
+            else:
+                res.add(nums1[i])
+                i+=1
+                j+=1
+        return res
+
